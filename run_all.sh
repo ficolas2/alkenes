2ad101d6142558b8b900056dab980afa677f5bee
ficolas2 <nicolasdepg@gmail.com>
2024-10-20 01:00:54 +0200

    feat: run_all

--- run_all.sh
+++ run_all.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+for i in {1..6}; do
+  for j in {0..4}; do
+    file="./output/ISO${i}-base${j}.gjf"
+    
+    if [[ -f $file ]]; then
+      echo "Running G09 for $file..."
+      G09 "$file"
+    else
+      echo "File $file not found, skipping..."
+    fi
+  done
+done
 
